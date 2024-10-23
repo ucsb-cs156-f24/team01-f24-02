25f124c9a26f3b99789aeb0d78a721c128d18073
Jeffery Wang <jefferywang@ucsb.edu>
2024-10-22 17:48:05 -0700

    added Articles class -jw

---
+++ src/main/java/edu/ucsb/cs156/example/entities/Articles.java
@@ -0,0 +1,28 @@
+package edu.ucsb.cs156.example.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+
+@Builder
+@Entity(name = "articles")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Articles {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String title;
+    private String url;
+    private String explanation;
+    private String email;
+    private LocalDateTime dateAdded;
+
+}
