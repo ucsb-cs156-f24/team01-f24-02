ac4179a09d0ebc23bae2a63b404c7296d056ef5d
Edwin Medrano <eduino@100-64-48-56.wireless.ucsb.edu>
2024-10-24 00:20:02 -0700

    EMV - created all controller tests, and ensured all mutations and lines were met

---
+++ src/test/java/edu/ucsb/cs156/example/controllers/UCSBOrganizationsControllerTests.java
@@ -0,0 +1,169 @@
+package edu.ucsb.cs156.example.controllers;
+
+import edu.ucsb.cs156.example.repositories.UserRepository;
+import edu.ucsb.cs156.example.testconfig.TestConfig;
+import edu.ucsb.cs156.example.ControllerTestCase;
+import edu.ucsb.cs156.example.entities.UCSBOrganizations;
+import edu.ucsb.cs156.example.repositories.UCSBOrganizationsRepository;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@WebMvcTest(controllers = UCSBOrganizationsController.class)
+@Import(TestConfig.class)
+public class UCSBOrganizationsControllerTests extends ControllerTestCase {
+    
+    @MockBean
+    UCSBOrganizationsRepository ucsbOrganizationsRepository;
+
+    @MockBean
+    UserRepository userRepository;
+
+    @Test
+    public void logged_out_users_cannot_get_all() throws Exception {
+        mockMvc.perform(get("/api/ucsborganizations/all"))
+                        .andExpect(status().is(200)); // logged out users can't get all
+    }
+
+    @WithMockUser(roles = { "USER" })
+    @Test
+    public void logged_in_users_can_get_all() throws Exception {
+        mockMvc.perform(get("/api/ucsborganizations/all"))
+                        .andExpect(status().is(200)); // logged
+    }
+
+    @WithMockUser(roles = { "USER" })
+    @Test
+    public void logged_in_user_can_get_all_ucsborganizations() throws Exception {
+        // arrange
+        UCSBOrganizations zetaPhiRho = UCSBOrganizations.builder()
+                    .orgCode("ZPR")
+                    .orgTranslationShort("ZETA PHI RHO")
+                    .orgTranslation("ZETA PHI RHO")
+                    .inactive(false)
+                    .build();
+
+        UCSBOrganizations skyDivingClub = UCSBOrganizations.builder()
+                    .orgCode("SKY")
+                    .orgTranslationShort("SKYDIVING CLUB")
+                    .orgTranslation("SKYDIVING CLUB AT UCSB")
+                    .inactive(false)
+                    .build();
+
+        UCSBOrganizations studentLife = UCSBOrganizations.builder()
+                    .orgCode("OSLI")
+                    .orgTranslationShort("STUDENT LIFE")
+                    .orgTranslation("OFFICE OF STUDENT LIFE")
+                    .inactive(false)
+                    .build(); 
+
+        UCSBOrganizations koreanRadio = UCSBOrganizations.builder()
+                    .orgCode("KRC")
+                    .orgTranslationShort("KOREAN RADIO CL")
+                    .orgTranslation("KOREAN RADIO CLUB")
+                    .inactive(false)
+                    .build();
+
+        ArrayList<UCSBOrganizations> expectedOrganizations = new ArrayList<>();
+        expectedOrganizations.addAll(Arrays.asList(zetaPhiRho, skyDivingClub, studentLife, koreanRadio));
+
+        when(ucsbOrganizationsRepository.findAll()).thenReturn(expectedOrganizations);
+
+        // act
+        MvcResult response = mockMvc.perform(get("/api/ucsborganizations/all"))
+                        .andExpect(status().isOk()).andReturn();
+
+        // assert
+        verify(ucsbOrganizationsRepository, times(1)).findAll();
+        String expectedJson = mapper.writeValueAsString(expectedOrganizations);
+        String responseString = response.getResponse().getContentAsString();
+        assertEquals(expectedJson, responseString);
+    }
+
+    // Tests for POST /api/ucsbdiningcommons...
+    @Test
+    public void logged_out_users_cannot_post() throws Exception {
+        mockMvc.perform(post("/api/ucsborganizations/post"))
+                        .andExpect(status().is(403));
+    }
+
+    @WithMockUser(roles = { "USER" })
+    @Test
+    public void logged_in_regular_users_cannot_post() throws Exception {
+            mockMvc.perform(post("/api/ucsborganizations/post"))
+                            .andExpect(status().is(403)); // only admins can post
+    }
+
+    @WithMockUser(roles = { "ADMIN", "USER" })
+    @Test
+    public void an_admin_user_can_post_a_new_ucsborganizations() throws Exception {
+            // arrange
+            UCSBOrganizations zetaPhiRho = UCSBOrganizations.builder()
+                    .orgCode("ZPR")
+                    .orgTranslationShort("ZETA PHI RHO")
+                    .orgTranslation("ZETA PHI RHO")
+                    .inactive(false)
+                    .build();
+
+            when(ucsbOrganizationsRepository.save(eq(zetaPhiRho))).thenReturn(zetaPhiRho);
+
+            // act
+            MvcResult response = mockMvc.perform(
+                            post("/api/ucsborganizations/post?name=zetaPhiRho&orgCode=ZPR&orgTranslationShort=ZETA PHI RHO&orgTranslation=ZETA PHI RHO&inactive=false")
+                                            .with(csrf()))
+                            .andExpect(status().isOk()).andReturn();
+
+            // assert
+            verify(ucsbOrganizationsRepository, times(1)).save(zetaPhiRho);
+            String expectedJson = mapper.writeValueAsString(zetaPhiRho);
+            String responseString = response.getResponse().getContentAsString();
+            assertEquals(expectedJson, responseString);
+    }
+
+    @WithMockUser(roles = { "ADMIN", "USER" })
+    @Test
+    public void an_admin_user_can_post_an_inactive_ucsborganization() throws Exception {
+        // arrange
+        UCSBOrganizations inactive = UCSBOrganizations.builder()
+                .orgCode("INACTIVE")
+                .orgTranslationShort("INACTIVE ORG")
+                .orgTranslation("INACTIVE ORGANIZATION")
+                .inactive(true)
+                .build();
+
+        when(ucsbOrganizationsRepository.save(eq(inactive))).thenReturn(inactive);
+
+        // act
+        MvcResult response = mockMvc.perform(
+                        post("/api/ucsborganizations/post?name=INACTIVE&orgCode=INACTIVE&orgTranslationShort=INACTIVE ORG&orgTranslation=INACTIVE ORGANIZATION&inactive=true")
+                                        .with(csrf()))
+                        .andExpect(status().isOk()).andReturn();
+
+        // assert
+        verify(ucsbOrganizationsRepository, times(1)).save(inactive);
+        String expectedJson = mapper.writeValueAsString(inactive);
+        String responseString = response.getResponse().getContentAsString();
+        assertEquals(expectedJson, responseString);
+    }
+}
